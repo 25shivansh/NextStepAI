dda2f67910cca9be439d21fe60ef43e9942096c9
25shivansh <shivansh.singh4work@gmail.com>
2025-08-06 19:01:24 +0530

    resume+cover-letter

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,9 +1,11 @@
+import { getResume } from '@/actions/resume'
+import ResumeBuilder from './_components/resume-builder';
 import React from 'react'
-
-const ResumePage = () => {
+const ResumePage = async () => {
+  const resume =await getResume();
   return (
-    <div>
-      Resume page
+    <div className='container mx-auto py-6'>
+      <ResumeBuilder initialContent={resume?.content}/>
     </div>
   )
 }
